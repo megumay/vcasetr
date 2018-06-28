6646c847d1205709643c6f28db045e35e5720243
megumay <40645970+megumay@users.noreply.github.com>
2018-06-28 11:24:49 +0200

    Update index.js

--- client/src/containers/faq/index.js
+++ client/src/containers/faq/index.js
@@ -39,7 +39,7 @@ class FaqList extends Component {
                         aria-expanded="true"
                         aria-controls={`collapse-${faqIndex}`}
                       >
-                        What is vCase.gg and how does it work?
+                        VCase.gg nedir ve nasıl çalışır?
                       </button>
                     </h5>
                   </div>
@@ -50,19 +50,19 @@ class FaqList extends Component {
                     data-parent="#accordion"
                   >
                     <div className="card-body">
-                      vCase.gg is the case opening site for{' '}
+                      vCase.gg, durumun açılış sitesidir.{' '}
                       <a href="http://vgo.gg/" target="_blank">
                         VGO items.{' '}
-                      </a>. vCases are opened with a vKey.
+                      </a>. vCases bir vKey ile açılır.
                       <br />
                       <br />
-                      VGO items are digital items generated using blockchain
-                      technology, so anyone can trade a VGO item to whoever they
-                      want, whenever they want, without any restrictions, trade
-                      holds, or fear of bannings. Every VGO item is
-                      one-of-a-kind and generated from an Ethereum smart
-                      contract. These items cannot ever be subjected to any
-                      trading restrictions such as trade holds or bans.
+                      VGO öğeleri blockchain kullanılarak üretilen dijital öğelerdir
+                       Teknoloji, böylece herkes, bir
+                       İstedikleri zaman, herhangi bir kısıtlama olmaksızın, ticaret yapmak isterler.
+                       yasaklar ya da korkular. Her VGO öğesi
+                       bir-bir-tür ve bir Ethereum smart dan üretilmiştir
+                       sözleşme. Bu ürünler hiç bir şeye tabi olamaz
+                       Ticaret tutarları veya yasaklar gibi ticaret kısıtlamaları.
                     </div>
                   </div>
                 </div>
@@ -76,7 +76,7 @@ class FaqList extends Component {
                         aria-expanded="true"
                         aria-controls={`collapse-${faqIndex}`}
                       >
-                        How do I get a vKey?
+                        Bir vKey i nasıl alabilirim?
                       </button>
                     </h5>
                   </div>
@@ -87,13 +87,13 @@ class FaqList extends Component {
                     data-parent="#accordion"
                   >
                     <div className="card-body">
-                      There are two ways to get a vKey:
+                      VKey almanın iki yolu vardır:
                       <ul>
                         <li>
-                          Purchase one from a marketplace that supports VGO
-                          items
+                          VGO yı destekleyen bir pazardan satın alın
+                           ürün
                         </li>
-                        <li>Get a vKey in a trade from another VGO user</li>
+                        <li>Başka bir VGO kullanıcısındaki ticarette bir vKey edinin</li>
                       </ul>
                     </div>
                   </div>
@@ -108,7 +108,7 @@ class FaqList extends Component {
                         aria-expanded="true"
                         aria-controls={`collapse-${faqIndex}`}
                       >
-                        Why is there only one type of vKey?
+                        Neden sadece bir tür vKey var?
                       </button>
                     </h5>
                   </div>
@@ -119,7 +119,7 @@ class FaqList extends Component {
                     data-parent="#accordion"
                   >
                     <div className="card-body">
-                      For simplicity, any vKey can open any vCase.
+                      Basitlik için, herhangi bir Anahtar herhangi bir Case açabilirsiniz.
                     </div>
                   </div>
                 </div>
@@ -133,7 +133,7 @@ class FaqList extends Component {
                         aria-expanded="true"
                         aria-controls={`collapse-${faqIndex}`}
                       >
-                        Why does it take so long to open a vCase?
+                        Bir Case açmak neden bu kadar uzun sürüyor?
                       </button>
                     </h5>
                   </div>
@@ -144,13 +144,13 @@ class FaqList extends Component {
                     data-parent="#accordion"
                   >
                     <div className="card-body">
-                      vCase.gg is based on the Ethereum blockchain for now,
-                      which brings many advantages over traditional case opening
-                      sites, mainly transparency. However until we migrate to
-                      the WAX Blockchain, case openings can take up to two
-                      minutes to complete because of the speed of Ethereum
-                      blockchain. The WAX Blockchain will be much faster and
-                      will make case openings instant.
+                      vCase.gg, şimdilik Ethereum blockchain i temel alıyor.
+                       geleneksel kasa açılışına göre birçok avantaj getiriyor
+                       siteler, esas olarak şeffaflık. Ancak biz taşınana kadar
+                       WAX Blockchain, kasa açıklıkları ikiye kadar alabilir
+                       Ethereum hızı nedeniyle tamamlanması dakikalar
+                       blockchain. WAX Blockchain çok daha hızlı olacak ve
+                       Vaka açılışlarını anında yapacaktır.
                     </div>
                   </div>
                 </div>
@@ -164,7 +164,7 @@ class FaqList extends Component {
                         aria-expanded="true"
                         aria-controls={`collapse-${faqIndex}`}
                       >
-                        Where do my VGO items go once a vCase is opened?
+                       VCase ım açıldığında, VGO öğelerim nereye gider?
                       </button>
                     </h5>
                   </div>
@@ -175,8 +175,9 @@ class FaqList extends Component {
                     data-parent="#accordion"
                   >
                     <div className="card-body">
-                      The VGO items that you receive from a vCase opening are
-                      sent to your{' '}
+                      Bir vCase açılışından aldığınız VGO öğeleri şunlardır:
+                       senin gönderdi
+                      {' '}
                       <a href="//trade.opskins.com/inventory" target="_blank">
                         OPSkins ExpressTrade Inventory
                       </a>.
@@ -193,7 +194,7 @@ class FaqList extends Component {
                         aria-expanded="true"
                         aria-controls={`collapse-${faqIndex}`}
                       >
-                        What can I do with my VGO items once I receive them?
+                       VGO öğelerimle bunları aldıktan sonra neler yapabilirim?
                       </button>
                     </h5>
                   </div>
@@ -204,10 +205,10 @@ class FaqList extends Component {
                     data-parent="#accordion"
                   >
                     <div className="card-body">
-                      You can trade your VGO items to another VGO user for free
-                      on VGO.gg, as long as you have their VGO Trade URL. You
-                      can also sell or trade them to any marketplace that
-                      supports VGO items.
+                      VGO öğelerinizi başka bir VGO kullanıcısına ücretsiz olarak aktarabilirsiniz.
+                       VGO.gg de, kendi VGO Ticari URL niz olduğu sürece. Sen
+                       ayrıca herhangi bir pazara satış veya ticaret yapabilir
+                       VGO öğelerini destekler.
                     </div>
                   </div>
                 </div>
@@ -232,8 +233,8 @@ class FaqList extends Component {
                     data-parent="#accordion"
                   >
                     <div className="card-body">
-                      VCases's opening odds are on average around 3x better than
-                      Steam's case opening odds.
+                      VCases in açılış oranları ortalama 3 kat daha iyi.
+                       Steam in kasa açılış oranları.
                     </div>
                   </div>
                 </div>
@@ -247,8 +248,8 @@ class FaqList extends Component {
                         aria-expanded="true"
                         aria-controls={`collapse-${faqIndex}`}
                       >
-                        How do I know that the vCase odds are what you claim
-                        they are?
+                        VCase oranlarının iddia ettiğiniz şey olduğunu nasıl bilebilirim?
+                         onlar?
                       </button>
                     </h5>
                   </div>
@@ -259,11 +260,11 @@ class FaqList extends Component {
                     data-parent="#accordion"
                   >
                     <div className="card-body">
-                      You can check the results of all vCase openings because
-                      all items generated from vCases are recorded on the
-                      Ethereum blockchain. Each item generated links back to a
-                      blockchain transaction for full transparency. For this
-                      reason, VGO items also cannot be duplicated.
+                      Tüm vCase açılışlarının sonuçlarını kontrol edebilirsiniz çünkü
+                       vCases ten üretilen tüm öğeler
+                       Eteryum blok zinciri. Her bir öğe, bir
+                       Tam şeffaflık için blockchain işlemi. Bunun için
+                       Nedeni, VGO öğeleri de çoğaltılamaz.
                     </div>
                   </div>
                 </div>
@@ -277,8 +278,8 @@ class FaqList extends Component {
                         aria-expanded="true"
                         aria-controls={`collapse-${faqIndex}`}
                       >
-                        Can I lose my VGO items, or can VGO get shut down by
-                        Steam?
+                        VGO öğelerimi kaybedebilir miyim, yoksa VGO kapanabilir mi?
+                         Buhar?
                       </button>
                     </h5>
                   </div>
@@ -289,11 +290,11 @@ class FaqList extends Component {
                     data-parent="#accordion"
                   >
                     <div className="card-body">
-                      No. Since VGO uses blockchain technology, neither Steam or
-                      anyone else can shut it down. If the OPSkins ExpressTrade
-                      website disappeared tomorrow, the data behind the items
-                      would still exist and could be rendered with graphics to
-                      showcase the unique properties of the items.
+                      Hayır. VGO blockchain teknolojisini kullandığından, ne Steam ne de
+                       başkası kapatabilir. OPSkins ExpressTrade varsa
+                       web sitesi yarın ortadan kayboldu, öğelerin arkasındaki veriler
+                       hala var olacak ve grafiklerle işlenebilecek
+                       öğelerin benzersiz özelliklerini sergileyin.
                     </div>
                   </div>
                 </div>
@@ -307,7 +308,7 @@ class FaqList extends Component {
                         aria-expanded="true"
                         aria-controls={`collapse-${faqIndex}`}
                       >
-                        Why is there a minimum case opening quantity?
+                        Minimum kasa açılış miktarı neden var?
                       </button>
                     </h5>
                   </div>
@@ -318,15 +319,15 @@ class FaqList extends Component {
                     data-parent="#accordion"
                   >
                     <div className="card-body">
-                      Because vCase is operating on the Ethereum blockchain at
-                      this time, there is a minimum case opening quantity due to
-                      ETH gas fees. Once we migrate to the WAX Blockchain, these
-                      minimums will change
+                      VCase, Ethereum blockchain üzerinde çalıştığından
+                       bu sefer, nedeniyle minimum bir kasa açılış miktarı
+                       ETH gaz ücretleri. WAX Blockchaine geçtikten sonra, bunlar
+                       minimumlar değişecek
                     </div>
                   </div>
                 </div>
                 <h2 className="sub-header">
-                  How can I build my own vCase opening site?
+                  Kendi vCase açılış sitemi nasıl oluşturabilirim?
                 </h2>
                 <div className="card">
                   <div className="card-header" id={`heading-${faqIndex}`}>
@@ -338,7 +339,7 @@ class FaqList extends Component {
                         aria-expanded="true"
                         aria-controls={`collapse-${faqIndex}`}
                       >
-                        Can I create my own vCase.gg case opening site?
+                        Kendi vCase.gg vaka açılış sitemi oluşturabilir miyim?
                       </button>
                     </h5>
                   </div>
@@ -349,7 +350,7 @@ class FaqList extends Component {
                     data-parent="#accordion"
                   >
                     <div className="card-body">
-                      Yes. The vCase code is open source and available on our{' '}
+                      Evet. VCase kodu açık kaynaklıdır ve{' '}
                       <a href="https://github.com/vgoskins" taget="_blank">
                         GitHub
                       </a>.
@@ -366,7 +367,7 @@ class FaqList extends Component {
                         aria-expanded="true"
                         aria-controls={`collapse-${faqIndex}`}
                       >
-                        Is there an affiliate program for vCase sites?
+                        VCase siteleri için bir ortaklık programı var mı?
                       </button>
                     </h5>
                   </div>
@@ -377,12 +378,12 @@ class FaqList extends Component {
                     data-parent="#accordion"
                   >
                     <div className="card-body">
-                      Yes. All sites that adopt the vCase functionality will
-                      automatically earn a 5% affiliate fee, paid to you in
-                      real-time in ETH. Because the vCase affiliate program is
-                      smart-contract based, when someone uses a vKey to open a
-                      vCase on your site, your payment will be sent instantly to
-                      your Ethereum address.
+                     Evet. VCase işlevselliğini benimseyen tüm siteler
+                       otomatik olarak size ödenen% 5 lik bir ortaklık ücreti kazanırsınız.
+                       ETH de gerçek zamanlı. VCase ortaklık programı olduğundan
+                       Akıllı sözleşme tabanlı, birisi bir vKey i açmak için
+                       Sitenizde vCase, ödemeniz anında gönderilecektir
+                       Ethereum adresiniz.
                     </div>
                   </div>
                 </div>
